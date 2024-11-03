ce7a78ce2c45dfdcbe77fab715d7e55df0e14f5a
chrod <chrod.dev@gmail.com>
2024-11-03 12:09:09 -0500

    Add change-product-fk migration

---
+++ migrations/20241029204848-change-product-fk.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface) {
+		// Eliminar columna 'categoryId' (antigua clave foránea)
+		await queryInterface.removeColumn('product', 'categoryId');
+
+		// Agregar restricción de clave foránea a 'categoryProductId'
+		await queryInterface.addConstraint('product', {
+			fields: ['categoryProductId'],
+			type: 'foreign key',
+			name: 'fk_product_categoryProductId',
+			references: {
+				table: 'category_product',
+				field: 'id',
+			},
+			onUpdate: 'CASCADE',
+			onDelete: 'CASCADE',
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		// Quitar restricción de clave foránea de 'categoryProductId'
+		await queryInterface.removeConstraint(
+			'product',
+			'fk_product_categoryProductId',
+		);
+
+		// Restaurar columna 'categoryId' como clave foránea
+		await queryInterface.addColumn('product', 'categoryId', {
+			type: Sequelize.UUID,
+			allowNull: true,
+			references: {
+				model: 'category_product',
+				key: 'id',
+			},
+			onUpdate: 'CASCADE',
+			onDelete: 'CASCADE',
+		});
+	},
+};
