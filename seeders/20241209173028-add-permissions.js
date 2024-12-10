a793cddcc27f2464db493e61c6ebfc4036593174
chrod <chrod.dev@gmail.com>
2024-12-09 22:03:13 -0500

    Add seeder to populate permission table

---
+++ seeders/20241209173028-add-permissions.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface) {
+		const items = [
+			'product',
+			'user',
+			'customer',
+			'billing',
+			'estimate',
+			'permission',
+			'stock',
+			'transaction',
+			'dashboard',
+		];
+
+		await queryInterface.bulkInsert(
+			'permission',
+			items
+				.map(table => {
+					return [
+						{
+							name: `${table}-read`,
+							createdDate: new Date(),
+							updatedDate: new Date(),
+						},
+						{
+							name: `${table}-create`,
+							createdDate: new Date(),
+							updatedDate: new Date(),
+						},
+						{
+							name: `${table}-update`,
+							createdDate: new Date(),
+							updatedDate: new Date(),
+						},
+						{
+							name: `${table}-delete`,
+							createdDate: new Date(),
+							updatedDate: new Date(),
+						},
+					];
+				})
+				.flat(),
+			{},
+		);
+	},
+
+	async down(queryInterface) {
+		await queryInterface.bulkDelete('permission', null, {});
+	},
+};
