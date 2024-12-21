5ac00e4953386b4d77233b10e43596432fe7cda8
chrod <chrod.dev@gmail.com>
2024-12-20 22:10:37 -0500

    Update rename-estimate-to-quote migration

--- migrations/20241220221047-rename-estimate-to-quote.js
+++ migrations/20241220221047-rename-estimate-to-quote.js
@@ -4,6 +4,9 @@
 module.exports = {
 	async up(queryInterface) {
 		await queryInterface.renameTable('estimate', 'quote');
+		await queryInterface.renameColumn('quote', 'createdById', 'createdBy');
+		await queryInterface.renameColumn('quote', 'updatedById', 'updatedBy');
+
 		await queryInterface.renameTable('estimate_item', 'quote_item');
 		await queryInterface.renameColumn('quote_item', 'estimateId', 'quoteId');
 	},
@@ -11,6 +14,9 @@ module.exports = {
 	async down(queryInterface) {
 		await queryInterface.renameColumn('quote_item', 'quoteId', 'estimateId');
 		await queryInterface.renameTable('quote_item', 'estimate_item');
+
+		await queryInterface.renameColumn('quote', 'createdBy', 'createdById');
+		await queryInterface.renameColumn('quote', 'updatedBy', 'updatedById');
 		await queryInterface.renameTable('quote', 'estimate');
 	},
 };
