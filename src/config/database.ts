b7a11c16c1371e64f0f45a257874757570475467
chrod <chrod.dev@gmail.com>
2025-03-07 13:14:16 -0500

    Update sequelize config

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,4 +6,10 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = ENV;
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 	host: DB_HOST,
 	dialect: 'postgres',
+	pool: {
+		max: 5,
+		min: 1,
+		acquire: 30000,
+		idle: 10000,
+	},
 });
